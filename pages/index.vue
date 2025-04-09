f3ca9d3cf3145fcc09e7e5b9bc6ab5a52c1dbce7
maxtyrchan <matyrchan@googlemail.com>
2025-04-09 15:53:31 +0200

    Übung 2

--- pages/index.vue
+++ pages/index.vue
@@ -3,19 +3,11 @@
     <div class="search-container">
       <div class="search-wrapper">
         <input
-          v-model="store.searchQuery"
           type="text"
           placeholder="Search Pokémon..."
           class="search-input"
         />
-        <button
-          v-if="store.searchQuery"
-          @click="store.clearSearchQuery()"
-          class="clear-button"
-          aria-label="Clear search"
-        >
-          ✕
-        </button>
+        <button class="clear-button" aria-label="Clear search">🗑️</button>
       </div>
     </div>
 
@@ -27,22 +19,7 @@
         Compare Pokémon
       </NuxtLink>
     </div>
-
-    <div v-if="store.allPokemon.length" class="pokemon-grid">
-      <PokemonCard
-        v-for="pokemon in store.filteredPokemon"
-        :key="pokemon.id"
-        :pokemon="pokemon"
-      />
-    </div>
-    <div v-else class="text-center text-gray-600">Loading Pokémon...</div>
   </div>
 </template>
 
-<script setup>
-const store = usePokemonStore();
-
-onMounted(() => {
-  store.fetchAllPokemon();
-});
-</script>
+<script setup></script>
