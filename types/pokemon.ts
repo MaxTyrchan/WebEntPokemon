95fa8fb3593ff97906ca6f8dbe8da0fb19fbbf9f
Wimmer Simon (XC-CT/EMT4) <Simon.Wimmer@de.bosch.com>
2025-04-22 14:38:14 +0200

    feat: add types and stats to Pokémon data, update dev script in package.json

--- types/pokemon.ts
+++ types/pokemon.ts
@@ -3,4 +3,11 @@ export interface Pokemon {
   name: string;
   height: number;
   weight: number;
+  types: string[];
+  stats: {
+    base_stat: number;
+    stat: {
+      name: string;
+    };
+  }[];
 }
